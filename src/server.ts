06172b986262e51d53c2fe78d555f172d6df1df1
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2024-03-15 21:59:15 +0530

    added error report to server's unhandled rejections and exceptions

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,20 @@ config({ path: `${__dirname}/../.env` })
 
 import mongoose from 'mongoose'
 import app from './app'
+import { sendEmail } from './utils/Email'
+
+process.on('uncaughtException', async (error) => {
+    await sendEmail(
+        `${process.env.ADMIN_EMAIL}`,
+        'Uncaught Exception in BlazeBasket api',
+        `
+        Error Name: ${error.name}<br>
+        Error Message: ${error.message}<br>
+        Error Stack: ${error.stack}<br>
+        `
+    )
+    process.exit(1)
+})
 
 // connect to database
 mongoose
@@ -13,4 +27,22 @@ mongoose
 // starting up the server
 const server = app.listen(process.env.PORT, () => {
     console.log('application is running on port 5500')
+})
+
+process.on('unhandledRejection', async function (error: any) {
+    console.log('inside')
+    await sendEmail(
+        `${process.env.ADMIN_EMAIL}`,
+        '⚠ Uncaught Rejection in BlazeBasket api',
+        `
+        Error Name: ${error.name}<br>
+        Error Message: ${error.message}<br>
+        Error Stack: ${error.stack}<br>
+
+        <h4>Server is shut down</h4>
+        `
+    )
+    server.close(() => {
+        process.exit(1)
+    })
 })
