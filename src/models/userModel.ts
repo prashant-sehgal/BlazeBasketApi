5b22418bd3245a2f87844681f1a7b2e9db38bde8
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2024-07-09 23:30:01 +0530

    typo resolved

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: [true, 'user mush have an email address'],
+        required: [true, 'user must have an email address'],
         unique: true,
     },
     password: {
