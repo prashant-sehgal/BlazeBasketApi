b084b7e341364fe51dab143093045a0ee23b9b6c
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2024-07-10 23:26:17 +0530

    added amount virtual property

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -7,45 +7,55 @@ interface OrderInterface {
     paymentStatus: 'pending' | 'completed'
     paymentIntentId: string
     sessionId: string
+    amount: number
 }
 
-const orderSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: [true, 'order must belong to a user'],
-    },
-    products: {
-        type: [
-            {
-                product: {
-                    type: mongoose.Types.ObjectId,
-                    ref: 'Product',
-                    requried: [true, 'order must contain one or more products'],
+const orderSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: [true, 'order must belong to a user'],
+        },
+        products: {
+            type: [
+                {
+                    product: {
+                        type: mongoose.Types.ObjectId,
+                        ref: 'Product',
+                        requried: [
+                            true,
+                            'order must contain one or more products',
+                        ],
+                    },
+                    quantity: {
+                        type: Number,
+                        default: 1,
+                    },
                 },
-                quantity: {
-                    type: Number,
-                    default: 1,
-                },
-            },
-        ],
+            ],
+        },
+        paymentIntentId: String,
+        sessionId: String,
+        completed: {
+            type: Boolean,
+            default: false,
+        },
+        paymentStatus: {
+            type: String,
+            enum: ['pending', 'completed'],
+            default: 'pending',
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
     },
-    paymentIntentId: String,
-    sessionId: String,
-    completed: {
-        type: Boolean,
-        default: false,
-    },
-    paymentStatus: {
-        type: String,
-        enum: ['pending', 'completed'],
-        default: 'pending',
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-})
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+)
 
 orderSchema.pre(/^find/, function (this: any) {
     this.populate({
@@ -56,6 +66,14 @@ orderSchema.pre(/^find/, function (this: any) {
         path: 'products.product',
         select: 'title price images',
     })
+})
+
+orderSchema.virtual('amount').get(function () {
+    let amount = 0
+    this.products.forEach((productObject: any) => {
+        amount += productObject.product.price * productObject.quantity
+    })
+    return amount
 })
 
 const Order = mongoose.model<OrderInterface>('Order', orderSchema)
